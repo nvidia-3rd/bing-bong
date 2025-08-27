dfead13a00dfc0a3c5ed50a7dc78c24d99856c00
jinsu <moonlt93@github.com>
2025-08-27 10:11:39 +0900

    chore: ui 수정 및 리드미 사진 첨부

--- bing_bong/client/app.py
+++ bing_bong/client/app.py
@@ -54,23 +54,9 @@ with right:
 # 하단 푸터
 st.markdown(
     """
-    <style>
-    .bb-footer {
-        display: flex;
-        justify-content: flex-start;  /* 왼쪽 정렬 */
-        gap: 24px;                    /* 아이템 간 간격 */
-        align-items: center;          /* 세로 정렬 */
-        padding: 12px 0;
-        color: #111;
-    }
-    .bb-footmark {
-        font-weight: bold;
-    }
-    </style>
-
     <div class="bb-footer">
-      <div>2025</div> <br/>
-      <div>Team BingBong<br/>표정 인식 공감 봇</div> <br/>
+      <div>2025</div>
+      <div>Team BingBong<br/>표정 인식 공감 봇</div>
       <div class="bb-footmark">BingBong</div>
     </div>
     """,
